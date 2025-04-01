fb9ebe6b91fb68d1257a3c6ea72eb0971d93c3a4
chihebdk <chihebd@gmail.com>
2025-04-01 19:16:27 -0400

    Update component via Octokit

--- src/components/Component.tsx
+++ src/components/Component.tsx
@@ -1,18 +1,1 @@
-import React, { useState } from 'react';
-
-const MyComponent = () => {
-  const [value, setValue] = useState('');
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
-
-  return (
-    <div>
-      <label>Enter some text:</label>
-      <input type="text" value={value} onChange={handleChange} />
-    </div>
-  );
-};
-
-export default MyComponent;
+Hello
