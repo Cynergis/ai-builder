97f401512a8948c43b20566e4e85d0cbb184133b
chihebdk <chihebd@gmail.com>
2025-04-30 12:02:20 -0400

    Update component via Octokit

--- src/components/Component.tsx
+++ src/components/Component.tsx
@@ -1,1 +1,17 @@
-Hello
+import React from 'react'
+
+const Input = ({ label, ...props }) => {
+  return (
+    <div className="flex flex-col space-y-2">
+      <label htmlFor={props.id} className="text-sm font-medium">
+        {label}
+      </label>
+      <input
+        className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        {...props}
+      />
+    </div>
+  )
+}
+
+export default Input
